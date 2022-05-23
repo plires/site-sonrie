0e44046a6883553e43f7cdb7cd22515a247af85f
Pablo Lires <pablolires@gmail.com>
2022-05-23 11:39:44 -0300

    archivo nuevo en carpeta includes. se inserta en index.php

---
+++ includes/grados.php
@@ -0,0 +1,52 @@
+<!-- Grados -->
+<section class="container-fluid grados">
+	<div class="container p-0">
+		<div class="row">
+
+			<div class="col-md-12">
+				<h2 data-aos="fade-up">Los tres grados de bienestar según nuestra institución</h2>
+				<p data-aos="fade-up">
+					Los sentimientos de bienestar personal son fundamentales para nuestra salud en general, permitiéndonos superar las dificultades y lograr las cosas que queremos en la vida. Nos debemos a nosotros mismos vivir nuestras vidas al máximo mientras tenemos la oportunidad, pero a veces podemos necesitar un poco de ayuda para hacerlo
+				</p>
+			</div>
+
+		  <div data-aos="fade-up" class="col-sm-6 col-lg-4 grado_individual">
+		  	<div class="content_grado">
+		  		<img class="img-fluid" src="./img/grado1.png" alt="grado 1">
+		  		<div class="content_data">
+			  		<h3>GRADO 1<br>impacto individual</h3>
+			  		<p>
+			  			1&ordm; Acceso a la educación, la salud, la alimentación, la vivienda o los bienes de consumo, entre otras cosas. 
+			  		</p>
+		  		</div>
+		  	</div>
+		  </div>
+
+		  <div data-aos="fade-up" class="col-sm-6 col-lg-4 grado_individual">
+		  	<div class="content_grado">
+		  		<img class="img-fluid" src="./img/grado2.png" alt="grado 2">
+		  		<div class="content_data">
+			  		<h3>GRADO 2<br>impacto grupal</h3>
+			  		<p>
+			  			2&ordm; El incremento en la calidad de vida en aspectos físicos, mentales, emocionales y sociales.
+			  		</p>
+		  		</div>
+		  	</div>
+		  </div>
+
+		  <div data-aos="fade-up" class="col-sm-6 col-lg-4 grado_individual">
+		  	<div class="content_grado">
+		  		<img class="img-fluid" src="./img/grado3.png" alt="grado 3">
+		  		<div class="content_data">
+			  		<h3>GRADO 3<br>impacto cultural</h3>
+			  		<p>
+			  			3&ordm; Intensamente, es la integración completa de los estados de bienestar físico, mental y espiritual. 
+			  		</p>
+		  		</div>
+		  	</div>
+		  </div>
+
+		</div>
+	</div>
+</section>
+<!-- Grados end -->
